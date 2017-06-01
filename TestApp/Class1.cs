4ca26d785125f08c2f0e608cbab1b1bd05a642c4
zpw <729650170@qq.com>
2017-06-01 20:41:16 +0800

    1

--- TestApp/Class1.cs
+++ TestApp/Class1.cs
@@ -10,7 +10,7 @@ namespace TestApp
     {
         public string Name { get; set; }
 
-        public int Id { get; set; }
+        public int Id {    get; set; }
         public int Sex { get; set; }
     }
 }
